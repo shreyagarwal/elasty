afd76893b04152e1cf6c3ff71fb182c7a13b5f93
shrey agarwal <shrey.agarwal@paytm.com>
2016-08-10 16:40:26 +0530

    cli satisfactorily added

--- README.md
+++ README.md
@@ -2,12 +2,49 @@
 elastic CLI for unix mac
 
 
+# Feed Messages in Elasticsearch from RabbitMq
+
+# Config object
+ - **host** : Can be single value like `http://localhost:9200` , `https://localhost:9200` or can be an array to indicate cluster `['http://10.0.0.1:9200', 'http://10.0.0.2:9200'] . NOTE: Please specify full uri, i.e. with protocol and port.
+ - **reqopts** : All the headers mentioned here are passed As-is in node HTTP request. NOTE : the request is created with this object and values like uri, host, etc. are overriden later.
+ - **bulkQueueThreshold** : While doing bulk operation in ES, Threshold count is checked before each bulk op. If value returned for any of the nodes is > than this value, then operation wont be done on that node, and another node will be tried. Default : 2
+ - **bulkThresholdRetries** : If bulk op request fails repeatedly on each node, then after max tries, the operation is delared failed. 0 or negative value indicates retry forever. Default : no. of hosts * bulkQueueThreshold * 2 . 
+ - **bulkThresholdRetryInterval** : Retry interval for bulk operations, if it fails due to whatever reason, value in Milliseconds. Default : 2000 
+
+# Bulk insert routine
+Requests from RabbitMq are pulled and flusehed when :
+ - The max size of bulk query ( 10Mb default ) is reached either with a single message or multiple.
+ - Max timeout is reached for a message. This will lead to a flush of whatever we have.
+ - Max documents limit is reached : is calculated by number of rows / 2 taking an average that insert, update requests are 2 liners whereas delete is 1 liner. 
+
+### Algo
+- Lets not Ack unless the message has been sent to ES.
+- Prefetch increase or decrease will only throttle client which is not important here.
+
+- At each message, count number of operations. There can be 4 types of operations : Create, index, update, delete. Parse each line, and see what the key is . Count operations accordingly.
+- check
+    - If total unacked message buffer > the buffer to flush ... normally keep this as prefetch_size only
+    - If total number of unacked message > the messages to flush ... Normally keep this as PREFETCH COUNT only
+
+=> Apart from it , if x ms have elapsed after oldest message , then flush it please ... setting timer per second isnot a nice idea.. 
+A setting should say how long can a message survive if not flushed. Lets keep a timer HALF of that time...
+
+
+
 #ToDo / Expectations
 
- - Curses / Top type tool to see Stats
- - Indices , movement of shards in reallocation
- - Q Threshold Alerts
- - Mapping update
- - Merges and Segmentation State
- - Heap Stats
- - Query Help : Hits, time taken, etc.
+ - CLI
+     + Threadpool check
+     + Single Level check on ES , as what all the problems can be
+     + Ncurses type tool to give all info
+     + replica change of index
+     + shard allocation ON/off
+     + term tool to edit Config ... Options and values
+     + Query Help : Hits, time taken, etc.
+
+ - Insert bulk data in ES
+     - connect with rabbitMq and Start getting messages
+
+ - Create brew , Deb file service
+
+
--- elasty.go
+++ elasty.go
@@ -1,44 +1,267 @@
 package main
 
 import (
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-//    "io/ioutil"
-    "log"
-    "strings"
-    "flag"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/lunux2008/xulu"
+	"github.com/streadway/amqp"
+	"github.com/urfave/cli"
 )
+
+type esBulkCntrlStmnt struct {
+	_index             string `json:"_index,omitempty"`
+	_type              string `json:"_type,omitempty"`
+	_id                string `json:"_id,omitempty"`
+	_retry_on_conflict string `json:"_retry_on_conflict,omitempty"`
+	_version           string `json:"_version,omitempty"`
+}
+
+type esBulkStmntType struct {
+	s_delete esBulkCntrlStmnt `json:"delete"`
+	// s_create esBulkCntrlStmnt `json:"create,omitempty"`
+	// s_insert esBulkCntrlStmnt `json:"insert,omitempty"`
+	// s_update esBulkCntrlStmnt `json:"update,omitempty"`
+}
 
 func main() {
 
-    // CLI
-    bThreadpool := flag.Bool("threadpool", false, "Print threadpool info")
-    flag.Parse()
+	// processRaw(`{ "delete": { "_index": "website", "_type": "blog", "_id": "123" }}
+	//        { "create": { "_index": "website", "_type": "blog", "_id": "123" }}
+	//        { "title":    "My first blog post" }
+	//        { "index":  { "_index": "website", "_type": "blog" }}
+	//        { "title":    "My second blog post" }
+	//        { "update": { "_index": "website", "_type": "blog", "_id": "123", "_retry_on_conflict" : 3}}
+	//        { "doc" : {"title" : "My updated blog post"}}
+	//        `)
 
-    if bThreadpool {
-        
-        // call threadpool Uri
-        resp, err := http.Get("http://localhost:9200/_cat/thread_pool?v&h=id,pid,ip,host,bulk.active,bulk.queue")
-    
-        if err != nil {
-            // handle error
-            log.Fatal(err)
-        }
+	app := cli.NewApp()
+	app.Name = "elasty"
+	app.Version = "0.0.1"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Shrey Agarwal",
+			Email: "s@shreyagarwal.com",
+		},
+	}
+	app.Usage = "Elasticsearch toolbelt based on experience"
 
-        defer resp.Body.Close()
-    
-        body, err := ioutil.ReadAll(resp.Body)
-    
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
+	app.Commands = []cli.Command{
+		{
+			Name:    "threadpool",
+			Aliases: []string{"th"},
+			Usage:   "Show cluster threadpool",
+			Action: func(c *cli.Context) error {
 
-    fmt.Printf("Body is, %q", body)
-    */
+				return nil
+			},
+		},
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("completed task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "template",
+			Aliases: []string{"t"},
+			Usage:   "options for task templates",
+			Subcommands: []cli.Command{
+				{
+					Name:  "add",
+					Usage: "add a new template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("new task template: ", c.Args().First())
+						return nil
+					},
+				},
+				{
+					Name:  "remove",
+					Usage: "remove an existing template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("removed task template: ", c.Args().First())
+						return nil
+					},
+				},
+			},
+		},
+	}
 
-    t := "id   pid   ip        host      bulk.active bulk.queue"
+	app.Run(os.Args)
 
-    fmt.Printf("%q\n", strings.Fields(t))
 }
 
+func processRaw(rawData string) {
+
+	/*
+	   - Split each string
+	   - Json parse each string
+	   - Classify the operation
+	   - Count number of operations and number of lines
+	   - Add lines in buffer ( Check thread concurrency here )
+	   - Take a call whether to insert data in ES or not
+	       - Lock the data and insert it and empty it
+	*/
+
+	// split the data
+	// fmt.Printf("%d\n", len(strings.Split(rawData, "\n")))
+
+	var splits []string = strings.Split(rawData, "\n")
+	xulu.Use(splits)
+
+	// work on each split
+	fmt.Printf("Splitting in lines : %d\n", len(splits))
+
+	fmt.Printf("Marshalling Line : %q\n", splits[0])
+
+	// Json unmarshal
+	// var stmnt1 esBulkStmntType
+	// err := json.Unmarshal([]byte(splits[0]), &stmnt1)
+	// if err != nil {
+	// 	// panic(err)
+	// 	log.Fatalf("json.Unmarshal: %s", err)
+	// }
+
+	// var parsed interface{}
+	var parsed map[string]interface{}
+
+	err := json.Unmarshal([]byte(splits[0]), &parsed)
+	if err != nil {
+		panic(err)
+		log.Fatalf("json.Unmarshal interface: %s", err)
+	}
+
+	remarsh2, _ := json.Marshal(parsed)
+
+	// map[delete:map[_id:123 _index:website _type:blog]]
+	fmt.Println(string(remarsh2))
+	// fmt.Println("%q\n", parsed, map[delete)
+
+	// fmt.Printf("%q\n", parsed.(type))
+}
+
+func parseThreadPoolOutpu(bulkData string) {
+	/*
+	   t := "id   pid   ip        host      bulk.active bulk.queue"
+	   fmt.Printf("%q\n", strings.Fields(t))
+	*/
+
+}
+
+func hitEs(bulkData string) {
+	resp, err := http.Get("http://localhost:9200/_cat/thread_pool?v&h=id,pid,ip,host,bulk.active,bulk.queue")
+
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ThreadPool, %q", body)
+
+}
+
+// Initizlise rabbit Mq connection
+func initialize() {
+
+	// Reconnect on conection Close
+	c := make(chan *amqp.Error)
+	go func() {
+		err := <-c
+		log.Println("reconnect after 5 seconds: " + err.Error())
+
+		time.Sleep(5 * 1000 * time.Millisecond)
+
+		initialize()
+	}()
+
+	// Connects opens an AMQP connection from the credentials in the URL.
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("connection.open: %s", err)
+		panic("cannot connect")
+	}
+	conn.NotifyClose(c)
+	// defer conn.Close()
+	fmt.Printf("Connection open\n")
+
+	// Opening channel
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("channel.open: %s", err)
+		panic("cannot open channel")
+	}
+	ch.NotifyClose(c)
+	fmt.Printf("Channel open\n")
+
+	// Declare exchange
+	err = ch.ExchangeDeclare("go_ex1", "topic", true, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("channel.ExchangeDeclare: %s", err)
+		panic("cannot declare exchange")
+	}
+	fmt.Printf("Exchange configured\n")
+
+	// declare Queue
+	q, err := ch.QueueDeclare("go_q1", true, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("channel.QueueDeclare: %s", err)
+		panic("cannot declare q")
+	}
+	fmt.Printf("Q configured\n")
+	_ = q
+
+	// Q bind
+	err = ch.QueueBind("go_q1", "#", "go_ex1", false, nil)
+	if err != nil {
+		log.Fatalf("channel.QueueBind: %s", err)
+		panic("cannot QueueBind")
+	}
+	fmt.Printf("Q bound\n")
+
+	// Qos
+
+	// Set our quality of service.  Since we're sharing 3 consumers on the same
+	// channel, we want at least 3 messages in flight.
+	err = ch.Qos(1, 0, false)
+	if err != nil {
+		log.Fatalf("basic.qos: %v", err)
+	}
+
+	//consume
+	test_msgs, err := ch.Consume("go_q1", "go_q1_CONSUMER", false, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("basic.consume: %v", err)
+	}
+
+	go func() {
+		for each_msg := range test_msgs {
+			log.Println("Msg: %s", string(each_msg.Body[:]))
+
+			err = each_msg.Ack(false)
+			if err != nil {
+				log.Fatalf("Error in Ack: %v", err)
+			}
+		}
+	}()
+
+	// send it to Elasticsearch as soon as you receive it .. and wait on receiving
+
+}
